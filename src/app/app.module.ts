d98e003ee6e6ac5576be6953cc910be2e803893b
takepo <takeshima.nahoko+1@gmail.com>
2020-01-10 19:02:36 +0900

    chore: g c my-counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { StoreModule } from "@ngrx/store";
 import { counterReducer } from "./counter.reducer";
 
 import { AppComponent } from "./app.component";
+import { MyCounterComponent } from "./my-counter/my-counter.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
   providers: [],
   bootstrap: [AppComponent]
